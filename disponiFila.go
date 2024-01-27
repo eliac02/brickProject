5152ef47d5dfa691c1b8930a2bb8af44a2bbfd87
eliacortesi <eliacortesi278@gmail.com>
2024-01-27 18:02:41 +0100

    first commit

---
+++ disponiFila.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func disponiFila(g gioco, listaNomi string) {
+	fmt.Println("Hello, playground")
+}
---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+type gioco struct {
+	scatola map[string][2]string
+	tavolo  []fila
+}
+
+type mattoncino struct {
+	alpha string
+	beta  string
+	sigma string
+}
+
+type fila struct {
+	//componenti linkedList
+	nome            string
+	indiceCacofonia int
+}
+
+func main() {
+	fmt.Println("Hello, playground")
+	g := gioco{scatola: make(map[string][2]string), tavolo: make([]fila, 0)}
+	listaNomi := ""
+	disponiFila(g, listaNomi)
+}
