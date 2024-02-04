3f2ca43caa9c99b7299f8dfa99d70d724d83cd4f
eliacortesi <eliacortesi278@gmail.com>
2024-02-04 09:43:39 +0100

    edit disponiFilaMinima.go

--- disponiFilaMinima.go
+++ disponiFilaMinima.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
 type arco struct {
@@ -22,70 +21,16 @@ func disponiFilaMinima(g gioco, alpha, beta string) {
 	}
 
 	if alpha == beta {
-		enqueue(queue, alpha)
-		//elencoPredecessori[alpha] = "" //------
-
-		//esploro il grafo
-		for !isEmpty(queue) {
-			current := bottom(queue)
-			dequeue(queue)
-			//visited[current] = true // --------
-			for arco := range elencoArchi {
-				if arco.start == current && !visited[arco.end] {
-					enqueue(queue, arco.end)
-					visited[arco.end] = true
-					elencoPredecessori[arco.end] = arco.start
-				} else if arco.end == current && !visited[arco.start] {
-					enqueue(queue, arco.start)
-					visited[arco.start] = true
-					elencoPredecessori[arco.start] = arco.end
-				}
-			}
-		}
-
-		//ricostruisco la sequenza di forme
-		var sequenzaForme []string
-		for current := beta; current != ""; current = elencoPredecessori[current] {
-			sequenzaForme = append(sequenzaForme, current)
-			if elencoPredecessori[current] == beta {
-				sequenzaForme = append(sequenzaForme, beta)
-				break
-			}
-		}
-
-		// creare elenco dei mattoncini
-		var listaNomi string
-		for i := len(sequenzaForme) - 1; i > 0; i-- {
-			// devo controllare entrambi i campi di arco all'interno di elencoArchi
-			arcoCorrente := arco{sequenzaForme[i], sequenzaForme[i-1]}
-			if nome, exists := elencoArchi[arcoCorrente]; exists {
-				brick := "+" + nome + " "
-				listaNomi += brick
-			} else {
-				arcoCorrente = arco{sequenzaForme[i-1], sequenzaForme[i]}
-				nome := elencoArchi[arcoCorrente]
-				brick := "-" + nome + " "
-				listaNomi += brick
-			}
-		}
-		//stampo elencoArchi
-		if listaNomi == "" || !verificaFila(strings.Split(listaNomi, " "), g) {
-			fmt.Printf("non esiste fila da %s a %s\n", alpha, beta)
-			return
-		}
-		listaNomi = listaNomi[:len(listaNomi)-1]
-
-		//dispongo fila
-		disponiFila(g, listaNomi)
+		//implementare la funzione nel caso in cui alpha e beta siano uguali
 	} else {
 		enqueue(queue, alpha)
-		elencoPredecessori[alpha] = "" //------
+		elencoPredecessori[alpha] = ""
 
 		//esploro il grafo
 		for !isEmpty(queue) {
 			current := bottom(queue)
 			dequeue(queue)
-			visited[current] = true // --------
+			visited[current] = true
 			for arco := range elencoArchi {
 				if arco.start == current && !visited[arco.end] {
 					enqueue(queue, arco.end)
